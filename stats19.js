580581b28f967d14a1622224a041d66a39c93de3
Adam Reynolds (Intel) <adam.reynolds@intel.com>
2020-05-01 23:31:15 +0100

    Multi-year support

--- stats19.js
+++ stats19.js
@@ -4,14 +4,14 @@
 
 // const engineccChart = new dc.BarChart("#enginecc_chart");
 const rowChartDimensions = [
-  "year", "month", "hour","Day_of_Week",
-  "Accident_Severity","Number_of_Vehicles","Number_of_Casualties",
-   "1st_Road_Class", "Road_Type", "Speed_limit", "Junction_Detail", "Junction_Control", "2nd_Road_Class"
+  "year", "month", "hour", "Day_of_Week",
+  "Accident_Severity", "Number_of_Vehicles", "Number_of_Casualties",
+  "1st_Road_Class", "Road_Type", "Speed_limit", "Junction_Detail", "Junction_Control", "2nd_Road_Class"
   , "Pedestrian_Crossing-Human_Control", "Pedestrian_Crossing-Physical_Facilities", "Light_Conditions", "Weather_Conditions",
   "Road_Surface_Conditions", "Special_Conditions_at_Site", "Carriageway_Hazards", "Urban_or_Rural_Area", "Did_Police_Officer_Attend_Scene_of_Accident"
 ];
 const casualtiesRowChartDimensions = [
-  "Casualty_Class", "Sex_of_Casualty", "Age_Band_of_Casualty", "Casualty_Severity", "Pedestrian_Location", "Pedestrian_Movement", "Car_Passenger", "Bus_or_Coach_Passenger", 
+  "Casualty_Class", "Sex_of_Casualty", "Age_Band_of_Casualty", "Casualty_Severity", "Pedestrian_Location", "Pedestrian_Movement", "Car_Passenger", "Bus_or_Coach_Passenger",
   "Casualty_Type", "Casualty_Home_Area_Type", "Casualty_IMD_Decile"
 ];
 
@@ -35,120 +35,128 @@ const translate = (key, value) => {
 
 }
 let addChartText = null;
-d3.csv("dftRoadSafetyData_Accidents_2018.csv")
-  .then(accidents => d3.csv("dftRoadSafetyData_Casualties_2018.csv")
-    .then(casualties => d3.csv("dftRoadSafetyData_Vehicles_2018.csv")
-      .then(vehicles => {
-        // Create the vehicleLookUp
-        const vehiclesHashmap = {};
-        vehicles.forEach(d => {
-          d["Engine_Capacity_CC"] = (Math.round(+d["Engine_Capacity_(CC)"] / 100) * 100);
-          d["Was_Vehicle_Left_Hand_Drive"] = d["Was_Vehicle_Left_Hand_Drive?"];
-          if (vehiclesHashmap[d.Accident_Index])
-            vehiclesHashmap[d.Accident_Index].push(d);
-          else
-            vehiclesHashmap[d.Accident_Index] = [d]
-        })
-        const casualtiesHashmap = {};
-        casualties.forEach(d => {
-          // Allocate vehicle that hit them
-          d["vehicle"] = vehiclesHashmap[d.Vehicle_Reference];
-          if (casualtiesHashmap[d.Accident_Index])
-            casualtiesHashmap[d.Accident_Index].push(d);
-          else
-            casualtiesHashmap[d.Accident_Index] = [d]
-        })
+const yearToUse = "2015";
+const yearsToUse = window.location.href.indexOf("localhost") != -1 ? ["2013","2014","2015", "2016", "2017", "2018"] : ["2017", "2018"];
+// const yearsToUse = ["2013","2014","2015", "2016", "2017", "2018"];
+const accidentPromises = yearsToUse.map(y => d3.csv("dftRoadSafetyData_Accidents_" + y + ".csv"));
+const casualtyPromises = yearsToUse.map(y => d3.csv("dftRoadSafetyData_Casualties_" + y + ".csv"));
+const vehiclesPromises = yearsToUse.map(y => d3.csv("dftRoadSafetyData_Vehicles_" + y + ".csv"));
+Promise.all(accidentPromises).then(accidentsArr =>
+  Promise.all(casualtyPromises).then(casualtiesArr =>
+    Promise.all(vehiclesPromises).then(vehiclesArr => {
+      const accidents = [].concat.apply([], accidentsArr);
+      const casualties = [].concat.apply([], casualtiesArr);
+      const vehicles = [].concat.apply([], vehiclesArr);
+      // Create the vehicleLookUp
+      const vehiclesHashmap = {};
+      vehicles.forEach(d => {
+        d["Engine_Capacity_CC"] = (Math.round(+d["Engine_Capacity_(CC)"] / 100) * 100);
+        d["Was_Vehicle_Left_Hand_Drive"] = d["Was_Vehicle_Left_Hand_Drive?"];
+        if (vehiclesHashmap[d.Accident_Index])
+          vehiclesHashmap[d.Accident_Index].push(d);
+        else
+          vehiclesHashmap[d.Accident_Index] = [d]
+      })
+      const casualtiesHashmap = {};
+      casualties.forEach(d => {
+        // Allocate vehicle that hit them
+        d["vehicle"] = vehiclesHashmap[d.Vehicle_Reference];
+        if (casualtiesHashmap[d.Accident_Index])
+          casualtiesHashmap[d.Accident_Index].push(d);
+        else
+          casualtiesHashmap[d.Accident_Index] = [d]
+      })
 
-        accidents.forEach(d => {
-          d.casualties = casualtiesHashmap[d.Accident_Index] ? casualtiesHashmap[d.Accident_Index] : [];
-          d.vehicles = vehiclesHashmap[d.Accident_Index] ? vehiclesHashmap[d.Accident_Index] : [];
-        })
-        accidents.forEach(function (x) {
-          var dateArr = x.Date.split("/");
-          x.year = +dateArr[2];
-          x.month = +dateArr[1];
-          x.hour = x.Time.split(":")[0];
-        });
-        const ndx = crossfilter(accidents);
-        // Declare chart dimensions and groups holders
-        const chartDimensions = {},
-          chartGroups = {};
-        rowChartDimensions.forEach((d, i) => {
-          chartDimensions[d] = ndx.dimension((dim) => dim[d]);
+      accidents.forEach(d => {
+        d.casualties = casualtiesHashmap[d.Accident_Index] ? casualtiesHashmap[d.Accident_Index] : [];
+        d.vehicles = vehiclesHashmap[d.Accident_Index] ? vehiclesHashmap[d.Accident_Index] : [];
+      })
+      accidents.forEach(function (x) {
+        var dateArr = x.Date.split("/");
+        x.year = +dateArr[2];
+        x.month = +dateArr[1];
+        x.hour = x.Time.split(":")[0];
+      });
+      const ndx = crossfilter(accidents);
+      // Declare chart dimensions and groups holders
+      const chartDimensions = {},
+        chartGroups = {};
+      rowChartDimensions.forEach((d, i) => {
+        chartDimensions[d] = ndx.dimension((dim) => dim[d]);
 
-          chartGroups[d] = chartDimensions[d].group();
-          let height = chartGroups[d].size() * 15 < 200 ? 200 : chartGroups[d].size() * 15;
-          rowCharts[i]
-            .width(200)
-            .height(height)
-            .margins({ top: 20, left: 10, right: 10, bottom: 30 })
-            .dimension(chartDimensions[d])
-            .group(chartGroups[d])
-            // Title sets the row text
-            .label((e) => translate(d, e.key))
+        chartGroups[d] = chartDimensions[d].group();
+        let height = chartGroups[d].size() * 15 < 200 ? 200 : chartGroups[d].size() * 15;
+        rowCharts[i]
+          .width(200)
+          .height(height)
+          .margins({ top: 20, left: 10, right: 10, bottom: 30 })
+          .dimension(chartDimensions[d])
+          .group(chartGroups[d])
+          // Title sets the row text
+          .label((e) => translate(d, e.key))
 
-            .elasticX(true)
-            .xAxis()
-            .ticks(4);
-        });
+          .elasticX(true)
+          .xAxis()
+          .ticks(4);
+      });
 
-        casualtiesRowChartDimensions.forEach((d, i) => {
-          // Array based dimension set
-          chartDimensions[d] = ndx.dimension((dim) => dim.casualties.map(c => c[d]), true);
-          chartGroups[d] = chartDimensions[d].group();
-          let height = chartGroups[d].size() * 15 < 200 ? 200 : chartGroups[d].size() * 15;
-          casualtiesRowCharts[i]
-            .width(200)
-            .height(height)
-            .margins({ top: 20, left: 10, right: 10, bottom: 30 })
-            .dimension(chartDimensions[d])
-            .group(chartGroups[d])
-            // Title sets the row text
-            .label((e) => translate(d, e.key))
+      casualtiesRowChartDimensions.forEach((d, i) => {
+        // Array based dimension set
+        chartDimensions[d] = ndx.dimension((dim) => dim.casualties.map(c => c[d]), true);
+        chartGroups[d] = chartDimensions[d].group();
+        let height = chartGroups[d].size() * 15 < 200 ? 200 : chartGroups[d].size() * 15;
+        casualtiesRowCharts[i]
+          .width(200)
+          .height(height)
+          .margins({ top: 20, left: 10, right: 10, bottom: 30 })
+          .dimension(chartDimensions[d])
+          .group(chartGroups[d])
+          // Title sets the row text
+          .label((e) => translate(d, e.key))
 
-            .elasticX(true)
-            .xAxis()
-            .ticks(4);
-        });
-        vehiclesRowChartDimensions.forEach((d, i) => {
-          // Array based dimension set
-          chartDimensions[d] = ndx.dimension((dim) => dim.vehicles.map(c => c[d]), true);
-          chartGroups[d] = chartDimensions[d].group();
-          let height = chartGroups[d].size() * 15 < 200 ? 200 : chartGroups[d].size() * 15;
-          vRowCharts[i]
-            .width(200)
-            .height(height)
-            .margins({ top: 20, left: 10, right: 10, bottom: 30 })
-            .dimension(chartDimensions[d])
-            .group(chartGroups[d])
-            // Title sets the row text
-            .label((e) => translate(d, e.key))
+          .elasticX(true)
+          .xAxis()
+          .ticks(4);
+      });
+      vehiclesRowChartDimensions.forEach((d, i) => {
+        // Array based dimension set
+        chartDimensions[d] = ndx.dimension((dim) => dim.vehicles.map(c => c[d]), true);
+        chartGroups[d] = chartDimensions[d].group();
+        let height = chartGroups[d].size() * 15 < 200 ? 200 : chartGroups[d].size() * 15;
+        vRowCharts[i]
+          .width(200)
+          .height(height)
+          .margins({ top: 20, left: 10, right: 10, bottom: 30 })
+          .dimension(chartDimensions[d])
+          .group(chartGroups[d])
+          // Title sets the row text
+          .label((e) => translate(d, e.key))
 
-            .elasticX(true)
-            .xAxis()
-            .ticks(4);
-        });
-
-
-        const police_force_d = ndx.dimension(d => translate("Police_Force",d.Police_Force)),
-          police_force_g = police_force_d.group();
-        police_force.dimension(police_force_d).group(police_force_g);
+          .elasticX(true)
+          .xAxis()
+          .ticks(4);
+      });
 
 
-        const local_authority_district_d = ndx.dimension(d => translate("Local_Authority_(District)", d["Local_Authority_(District)"])),
-          local_authority_district_g = local_authority_district_d.group();
-        local_authority_district.dimension(local_authority_district_d).group(local_authority_district_g);
-        const local_authority_highway_d = ndx.dimension(d => translate("Local_Authority_(Highway)", d["Local_Authority_(Highway)"])),
-          local_authority_highway_g = local_authority_highway_d.group();
-        local_authority_highway.dimension(local_authority_highway_d).group(local_authority_highway_g);
+      const police_force_d = ndx.dimension(d => translate("Police_Force", d.Police_Force)),
+        police_force_g = police_force_d.group();
+      police_force.dimension(police_force_d).group(police_force_g);
 
-        const all = ndx.groupAll();
-        dc.dataCount(".dc-data-count")
-          .crossfilter(ndx)
-          .groupAll(all);
 
-        dc.renderAll();
-        addChartText = () => {
+      const local_authority_district_d = ndx.dimension(d => translate("Local_Authority_(District)", d["Local_Authority_(District)"])),
+        local_authority_district_g = local_authority_district_d.group();
+      local_authority_district.dimension(local_authority_district_d).group(local_authority_district_g);
+      const local_authority_highway_d = ndx.dimension(d => translate("Local_Authority_(Highway)", d["Local_Authority_(Highway)"])),
+        local_authority_highway_g = local_authority_highway_d.group();
+      local_authority_highway.dimension(local_authority_highway_d).group(local_authority_highway_g);
+
+      const all = ndx.groupAll();
+      dc.dataCount(".dc-data-count")
+        .crossfilter(ndx)
+        .groupAll(all);
+
+      dc.renderAll();
+      addChartText = () => {
         rowCharts.forEach((rc, i) =>
           rc
             .svg()
@@ -182,4 +190,5 @@ d3.csv("dftRoadSafetyData_Accidents_2018.csv")
         );
       };
       addChartText();
-      })));
+
+    })));
